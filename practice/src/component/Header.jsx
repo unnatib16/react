de7cbefcf72b4bb7ac388bb7e1c7be726322cabc
Unnati Bansal <bansalunnati279@gmail.com>
2025-08-01 19:28:40 +0530

    changes

--- practice/src/component/Header.jsx
+++ practice/src/component/Header.jsx
@@ -1,15 +1,32 @@
 import React, { useEffect,useState } from "react";
 import styles from "./Header.module.css"
 export default function Header(){
-    const items = ['Apple', 'Banana', 'Orange', 'Mango'];
-    return(
-        <>
-    <ul>
-  {items.map((item, index) => (
-    <li key={index}>{item}</li>
-  ))}
-</ul>
-</>
-    )}
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const printData =(e)=>{
+        e.preventDefault();
+        console.log('Form Submitted');
+        console.log(username);
+        console.log(email);
 
+        setUsername('');
+        setEmail('');
+    }
+        
+        return(
+        <>
+        <h1>Day 7</h1>
+        <form onSubmit={printData}>
+            <label>Username :- </label>
+            <input type="text" placeholder="username" value={username} onChange={(e)=>setUsername(e.target.value)}></input>
+            <br></br>
+            <label>Email :- </label>
+            <input type="email" placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+            <br></br>
+            <br></br>
+            <button type="submit">Submit</button>
+        </form>
+        </>
+    )
+}
 
